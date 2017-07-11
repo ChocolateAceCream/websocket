653b156fb3b444e0288b096bd18d100e2288cb63
Ubuntu <ubuntu@ip-172-31-53-254.ec2.internal>
2017-07-11 20:15:03 +0000

    switched to unicorn

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,12 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
+require 'rails/commands/server'
+module Rails
+  class Server
+    def default_options
+      super.merge(Host:  '172.31.53.254', Port: 3003)
+    end
+  end
+end
+
 require 'bundler/setup' # Set up gems listed in the Gemfile.
